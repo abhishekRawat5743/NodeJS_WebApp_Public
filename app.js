c0a6a009b79a5648960678cff8929e20ad8bbe50
Abhishek Rawat <abhishekrawa@IN-V57J4M62GT.local>
2025-05-29 11:15:49 +0530

    updated port

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const port = process.env.PORT || 3000;
 
-
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
-  
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-
-app.get('/about', (req,res) => {
-    res.sendFile(path.join(__dirname+'/about.html'));
-   
-  });
-
-app.listen(8081, () => {
-    console.log('Listening on port 8081');
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/about.html"));
 });
+
+app.listen(port, () => console.log(`Server running on ${port}`));
