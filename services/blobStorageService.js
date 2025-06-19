caffa66b65d740d8f13e29159b231d1cf2be433d
Abhishek Rawat <abhishekrawa@IN-V57J4M62GT.local>
2025-06-19 13:38:16 +0530

    updated key vault secret variable name

--- services/blobStorageService.js
+++ services/blobStorageService.js
@@ -2,7 +2,7 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 const fs = require("fs");
 require("dotenv").config();
 
-const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const connectionString = process.env.DB_CONNECTION_STRING;
 const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
 
 if (!connectionString) {
